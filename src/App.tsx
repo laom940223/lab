66e91ebcadb81c85fd781f3088a104aa36120ccf
Manuel Lara <smazzbass@gmail.com>
2023-06-29 13:38:53 -0600

    Added React Router and React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from 'antd'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
+  
+      <>
+        <Routes>
+            <Route path='/' element={ <Home/> } />
+
+        </Routes>
+
+      </>
   )
 }
 
